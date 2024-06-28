a098343f96992e1df14b2287cfd233c55426018f
KeK <cuddlelyholly@naver.com>
2024-06-28 17:12:44 +0900

    :white_check_mark: 6/27 상속 과제 : 기본 실습 문제 2 Student 클래스 작성완료

--- day12/section1/Prob2/Student.java
+++ day12/section1/Prob2/Student.java
@@ -6,6 +6,13 @@ public class Student {
   private int fee;
   private double returnFee;
 
+
+  protected static int a;
+  private static int b;
+  public static int c;
+
+
+///////constructor////////
   public Student(String name, String subject, int fee){
     this.name = name;
     this.subject = subject;
@@ -13,13 +20,18 @@ public class Student {
   }
 
   public void calcReturnFee(){
-
+    if(this.subject.equals("javaprogram")) {
+      this.returnFee = this.fee * 0.25;
+    } else if (this.subject.equals("jspprogram")){
+      this.returnFee = this.fee * 0.2;
+    } else {
+      System.out.println("그런 과정명은 없습니다.");
+    }
   };
 
   public void print(){
-
+    System.out.printf("%s 씨의 과정명은 %s 이고 교육비는 %d 이며 환급금은 %.1f 입니다.", this.name, this.subject, this.fee, this.returnFee);
   }
-
 
   public static void main(String args[]){
     Student stu = new Student("차은우", "jspprogram",
