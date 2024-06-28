fa570128991b4a65ed67055e85f099dea0661109
KeK <cuddlelyholly@naver.com>
2024-06-28 15:45:05 +0900

    :white_check_mark: 6/27 상속 과제 : 기본 실습 문제 1

--- day12/section1/Prob1/Alcohol.java
+++ day12/section1/Prob1/Alcohol.java
@@ -1,5 +1,32 @@
 package day12.section1.Prob1;
 
-public class Alcohol {
+public class Alcohol extends Drink {
 
+  private double alcper;
+
+  public Alcohol(String name, int price, int count, double alcper) {
+    super(name, price, count);
+    this.alcper = alcper;
+  }
+
+  @Override
+  public void printTitle() {
+    System.out.println("상품명(도수[%])\t\t단가\t\t수량\t\t금액");
+  }
+
+  @Override
+  public void printData() {
+    System.out.println(
+        super.getName() + "(" + alcper + ")   " + super.getPrice() + "  " + super.getCount() + "  "
+            + super.getTotalPrice());
+  }
+
+  ///////////////////////////
+  public double getAlcper() {
+    return alcper;
+  }
+
+  public void setAlcper(double alcper) {
+    this.alcper = alcper;
+  }
 }
--- day12/section1/Prob1/Drink.java
+++ day12/section1/Prob1/Drink.java
@@ -10,4 +10,41 @@ private int count;
     this.price = price;
     this.count = count;
   }
+
+  public void printTitle() {
+    System.out.println("상품명\t\t단가\t\t수량\t\t금액");
+  }
+
+  public int getTotalPrice() {
+    int totalPrice = price * count;
+    return totalPrice;
+  }
+  public void printData() {
+    int totalPrice = getTotalPrice();
+    System.out.println(name + "  " + price + "  " + count + "  " + totalPrice);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public void setCount(int count) {
+    this.count = count;
+  }
 }
